5f3b98a422e7832f3fefc5e43f3d04bf48a8b2d4
Bilal Afzaal <bafzaal@uwo.ca>
2021-04-04 21:32:37 -0400

    Added a profile header (hard coded values for now) to the profile page

--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
+import ProfileHeader from './ProfileHeader';
 
 export default function ProfilePage()
 {
     return(
-        <h1>Profile</h1>
+        <Grid>
+            <Grid.Column width={16}>
+                <ProfileHeader />
+            </Grid.Column>
+        </Grid>
     )
 }
 
