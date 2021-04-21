909f6d6e17c0b9ed6e03c752e53158ffec280a7c
Bilal Afzaal <bafzaal@uwo.ca>
2021-04-21 00:10:40 -0400

    Added vertical scrolling (currently implemented with a button but will change this later)

--- client-app/src/features/defects/dashboard/DefectDashboard.tsx
+++ client-app/src/features/defects/dashboard/DefectDashboard.tsx
@@ -1,7 +1,8 @@
 import { observer } from 'mobx-react-lite';
-import React, { useEffect } from 'react';
-import { Grid } from 'semantic-ui-react';
+import React, { useEffect, useState } from 'react';
+import { Button, Grid } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
+import { PagingParams } from '../../../app/models/pagination';
 import { useStore } from '../../../app/stores/store';
 import DefectFilters from './DefectFilters';
 import DefectList from './DefectList';
@@ -9,13 +10,21 @@ import DefectList from './DefectList';
 export default observer(function DefectDashboard()
 {
     const {defectStore} = useStore();
-    const {loadDefects, defectRegistry} = defectStore;
+    const {loadDefects, defectRegistry, setPagingParams, pagination} = defectStore;
+    const [loadingNext, setLoadingNext] = useState(false);
+
+    function handleGetNext()
+    {
+        setLoadingNext(true);
+        setPagingParams(new PagingParams(pagination!.currentPage + 1));
+        loadDefects().then(() => setLoadingNext(false));
+    }
 
     useEffect(() => {
         if(defectRegistry.size <= 1) loadDefects();
     }, [defectRegistry.size, loadDefects])
 
-    if(defectStore.loadingInitial)
+    if(defectStore.loadingInitial && !loadingNext)
     {
         return <LoadingComponent content="Loading Defects..." />
     }
@@ -23,7 +32,15 @@ export default observer(function DefectDashboard()
     return(
         <Grid>
             <Grid.Column width='10'>
-            <DefectList />             
+                <DefectList />
+                <Button 
+                    floated='right'
+                    content='More...'
+                    positive
+                    onClick={handleGetNext}
+                    loading={loadingNext}
+                    disabled={pagination?.totalPages === pagination?.currentPage}
+                />
             </Grid.Column>
             <Grid.Column width='6'>
                 <DefectFilters />
