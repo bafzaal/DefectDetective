d148d51cbd0e4c993a7056a281f7b203e7d551b3
Bilal Afzaal <bafzaal@uwo.ca>
2021-04-21 02:22:40 -0400

    Replaced the load more button with infinite scrolling

--- client-app/src/features/defects/dashboard/DefectDashboard.tsx
+++ client-app/src/features/defects/dashboard/DefectDashboard.tsx
@@ -1,49 +1,49 @@
 import { observer } from 'mobx-react-lite';
 import React, { useEffect, useState } from 'react';
-import { Button, Grid } from 'semantic-ui-react';
+import { Grid, Loader } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { PagingParams } from '../../../app/models/pagination';
 import { useStore } from '../../../app/stores/store';
 import DefectFilters from './DefectFilters';
 import DefectList from './DefectList';
+import InfiniteScroll from 'react-infinite-scroller';
 
-export default observer(function DefectDashboard()
-{
-    const {defectStore} = useStore();
-    const {loadDefects, defectRegistry, setPagingParams, pagination} = defectStore;
+export default observer(function DefectDashboard() {
+    const { defectStore } = useStore();
+    const { loadDefects, defectRegistry, setPagingParams, pagination } = defectStore;
     const [loadingNext, setLoadingNext] = useState(false);
 
-    function handleGetNext()
-    {
+    function handleGetNext() {
         setLoadingNext(true);
         setPagingParams(new PagingParams(pagination!.currentPage + 1));
         loadDefects().then(() => setLoadingNext(false));
     }
 
     useEffect(() => {
-        if(defectRegistry.size <= 1) loadDefects();
+        if (defectRegistry.size <= 1) loadDefects();
     }, [defectRegistry.size, loadDefects])
 
-    if(defectStore.loadingInitial && !loadingNext)
-    {
+    if (defectStore.loadingInitial && !loadingNext) {
         return <LoadingComponent content="Loading Defects..." />
     }
 
-    return(
+    return (
         <Grid>
             <Grid.Column width='10'>
-                <DefectList />
-                <Button 
-                    floated='right'
-                    content='More...'
-                    positive
-                    onClick={handleGetNext}
-                    loading={loadingNext}
-                    disabled={pagination?.totalPages === pagination?.currentPage}
-                />
+                <InfiniteScroll
+                    pageStart={0}
+                    loadMore={handleGetNext}
+                    hasMore={!loadingNext && !!pagination && pagination.currentPage < pagination.totalPages}
+                    initialLoad={false}
+                >
+                    <DefectList />
+                </InfiniteScroll>
             </Grid.Column>
             <Grid.Column width='6'>
                 <DefectFilters />
+            </Grid.Column>
+            <Grid.Column width={10}>
+                <Loader active={loadingNext} />
             </Grid.Column>
         </Grid>
     )
