597baec44c20f5896c56ef4dc8c9738333cdc946
Bilal Afzaal <bafzaal@uwo.ca>
2021-05-02 23:42:04 -0400

    Added styling to landing page

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -2,6 +2,7 @@ import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Header, Segment, Image, Button } from 'semantic-ui-react';
+import NavBar from '../../app/layout/NavBar';
 import { useStore } from '../../app/stores/store';
 import LoginForm from '../users/LoginForm';
 import RegisterForm from '../users/RegisterForm';
@@ -9,30 +10,49 @@ import RegisterForm from '../users/RegisterForm';
 export default observer(function HomePage() {
     const { userStore, modalStore } = useStore();
     return (
-        <Segment inverted textAlign='center' vertical className='masthead'>
-            <Container text>
-                <Header as='h1' inverted>
-                    <Image size='massive' src='/assets/logo2.png' alt='logo' style={{ marginBottom: 12 }} />
+        <div id="landing">
+            <div className="ui secondary stackable menu" id="menu">
+                <div className="item">
+                <Image size='mini' src='/assets/logo_red.png' alt='logo' />
+                </div>
+                <div className="right menu">
+                    <div className="item header"><a className="linkNav" href="/">Home</a></div>
+                    <div className="item header"><a className="linkNav" href="#">About</a></div>
+                    <div className="item">
+                        <button className="ui huge red button">Create Defect</button>
+                    </div>
+                </div>
+            </div>
+            <div className="ui text container">
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <Header as='h1' className="headerText" inverted>
                     Defect Detective
+                </Header>
+                <Header as='h3' id="subHeaderText" inverted>
+                    Collaborate with your team and work together to resolve defects.
                 </Header>
                 {userStore.isLoggedIn ? (
                     <>
                         <Header as='h2' inverted content='Welcome to Defect Detective' />
-                        <Button as={Link} to='/defects' size='huge' inverted>
+                        <Button as={Link} to='/defects' size='large' className="red">
                             Go to Defects!
                         </Button>
                     </>
                 ) : (
                     <>
-                        <Button onClick={() => modalStore.openModal(<LoginForm />)} size='huge' inverted>
-                            Login!
+                        <Button onClick={() => modalStore.openModal(<LoginForm />)} size='large' className="red">
+                            Login
                         </Button>
-                        <Button onClick={() => modalStore.openModal(<RegisterForm />)} size='huge' inverted>
-                            Register!
+                        <Button onClick={() => modalStore.openModal(<RegisterForm />)} size='large' className="white" inverted>
+                            Register
                         </Button>
                     </>
                 )}
-            </Container>
-        </Segment>
+            </div>
+        </div>
     )
 })
