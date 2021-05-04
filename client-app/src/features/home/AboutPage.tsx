48085b6130b7c748de3ae86c1907869b8061c1c6
Bilal Afzaal <bafzaal@uwo.ca>
2021-05-04 02:53:34 -0400

    Changed icons and added styling to about page

--- client-app/src/features/home/AboutPage.tsx
+++ client-app/src/features/home/AboutPage.tsx
@@ -8,41 +8,41 @@ export default observer(function AboutPage() {
             </header>
             <div id="About" className="ui stackable grid">
                 <div className="three column row">
-                    <div className="column">
-                        <i className="laptop huge icon"></i>
-                        <h2 className="ui big header">Fully Responsive</h2>
+                    <div className="column text-center">
+                        <i className="bug huge icon aboutIcon"></i>
+                        <h2 className="ui big header">Track Defects</h2>
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora doloribus ab enim cum nobis
         obcaecati aut, vitae laborum reprehenderit officia?</p>
                     </div>
-                    <div className="column">
-                        <i className="clock huge icon"></i>
-                        <h2 className="ui big header">Powerfull Admin</h2>
+                    <div className="column text-center">
+                        <i className="handshake outline huge icon aboutIcon"></i>
+                        <h2 className="ui big header">Teamwork</h2>
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora doloribus ab enim cum nobis
         obcaecati aut, vitae laborum reprehenderit officia?</p>
                     </div>
-                    <div className="column">
-                        <i className="world huge icon"></i>
-                        <h2 className="ui big header">Multi Language</h2>
+                    <div className="column text-center">
+                        <i className="comment outline huge icon aboutIcon"></i>
+                        <h2 className="ui big header">Live Chat</h2>
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora doloribus ab enim cum nobis
         obcaecati aut, vitae laborum reprehenderit officia?</p>
                     </div>
                 </div>
                 <div className="three column row">
-                    <div className="column">
-                        <i className="search huge icon"></i>
-                        <h2 className="ui big header">Redius Search</h2>
+                    <div className="column text-center">
+                        <i className="archive huge icon aboutIcon"></i>
+                        <h2 className="ui big header">Performance History</h2>
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora doloribus ab enim cum nobis
         obcaecati aut, vitae laborum reprehenderit officia?</p>
                     </div>
-                    <div className="column">
-                        <i className="id badge huge icon"></i>
-                        <h2 className="ui big header">Powerfull Admin</h2>
+                    <div className="column text-center">
+                        <i className="filter huge icon aboutIcon"></i>
+                        <h2 className="ui big header">Filter</h2>
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora doloribus ab enim cum nobis
         obcaecati aut, vitae laborum reprehenderit officia?</p>
                     </div>
-                    <div className="column">
-                        <i className="language huge icon"></i>
-                        <h2 className="ui big header">Multi Language</h2>
+                    <div className="column text-center">
+                        <i className="computer huge icon aboutIcon"></i>
+                        <h2 className="ui big header">Fully Responsive</h2>
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora doloribus ab enim cum nobis
         obcaecati aut, vitae laborum reprehenderit officia?</p>
                     </div>
